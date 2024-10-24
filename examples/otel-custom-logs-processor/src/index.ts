aec453dbad3281738474669cc29285c73c83190b
Hristo Kolev <hristo.a.kolev@gmail.com>
2024-10-24 15:02:22 +0300

    add custom logs processor example

---
+++ examples/otel-custom-logs-processor/src/index.ts
@@ -0,0 +1,35 @@
+import { start, type Config } from '@interopio/manager';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
+
+const config: Config = {
+  name: 'example',
+  port: 4356,
+  base: 'api',
+  auth_method: 'none',
+  auth_exclusive_users: ['example_user'],
+  store: {
+    type: 'mongo',
+    connection:
+      'mongodb://db_user:Password123$@localhost:27017/io_manager?authSource=admin',
+  },
+  token: {
+    secret: '<YOUR_SECRET>',
+  },
+  otel: {
+    enabled: true,
+    resource: {
+      serviceName: 'io-manager',
+    },
+    logs: {
+      enabled: true,
+      customProcessor: new SimpleLogRecordProcessor(
+        new OTLPLogExporter({
+          url: 'http://localhost:4318/v1/logs',
+        })
+      ),
+    },
+  },
+};
+
+start(config);
