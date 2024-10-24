b0eaee256b31cbfac22612e2e2817782058f904f
Hristo Kolev <hristo.a.kolev@gmail.com>
2024-10-24 15:55:06 +0300

    add otel-custom-metrics-exporter

---
+++ examples/otel-custom-metrics-exporter/src/index.ts
@@ -0,0 +1,32 @@
+import { start, Config } from '@interopio/manager';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+
+const config: Config = {
+  name: 'example',
+  port: 4356,
+  base: 'api',
+  auth_method: 'none',
+  auth_exclusive_users: ['example_user'],
+  store: {
+    type: 'mongo',
+    connection:
+      'mongodb://db_user:Password123$@localhost:27017/io_manager?authSource=admin',
+  },
+  token: {
+    secret: '<YOUR_SECRET>',
+  },
+  otel: {
+    enabled: true,
+    resource: {
+      serviceName: 'io-manager',
+    },
+    metrics: {
+      enabled: true,
+      customExporter: new OTLPMetricExporter({
+        url: 'http://localhost:4318/v1/metrics',
+      }),
+    },
+  },
+};
+
+start(config);
