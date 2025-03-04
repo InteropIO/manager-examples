1a07f796163cb255ca40eaae8e7f403a0bb818ac
Hristo Kolev <hristo.a.kolev@gmail.com>
2025-03-04 20:08:45 +0200

    Add global MD files to the format script

--- .maintenance/scripts/format.js
+++ .maintenance/scripts/format.js
@@ -1,3 +1,6 @@
+import path from 'node:path';
+import url from 'node:url';
+
 import { $ } from 'zx/core';
 import { program } from 'commander';
 
@@ -8,3 +11,8 @@ const options = program.parse().opts();
 await visitPackages(options.package, async () => {
   await $`npm run format`;
 });
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+$.cwd = path.resolve(__dirname, '../../');
+await $`npx -y prettier --write **/*.md`;
