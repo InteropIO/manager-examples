d7faccd54e3e91ed8db15b600fc4e7000291ca7e
Hristo Kolev <hristo.a.kolev@gmail.com>
2025-05-23 15:42:59 +0300

    add debug env variable

--- .maintenance/scripts/install-local.js
+++ .maintenance/scripts/install-local.js
@@ -4,12 +4,17 @@ import { program } from 'commander';
 import { visitPackages } from './helpers/visit-packages.js';
 import { useLocalRegistryNpmrc } from './helpers/use-npmrc.js';
 import { packageScope } from './helpers/variables.js';
+import process from 'node:process';
 
 const options = program.parse().opts();
 
 await visitPackages(options.package, async ({ packageJson }) => {
   await useLocalRegistryNpmrc(async () => {
-    await $`npm install`;
+    if (process.env.CI_DEBUG === '1') {
+      await $`npm install --loglevel verbose`;
+    } else {
+      await $`npm install`;
+    }
 
     const packageNames = Object.keys(packageJson.dependencies ?? {}).filter(
       (x) => x.startsWith(`${packageScope}/`)
@@ -18,7 +23,11 @@ await visitPackages(options.package, async ({ packageJson }) => {
     for (const packageName of packageNames) {
       const packageNameLatest = packageName + '@latest';
 
-      await $`npm install ${packageNameLatest}`;
+      if (process.env.CI_DEBUG === '1') {
+        await $`npm install --loglevel verbose ${packageNameLatest}`;
+      } else {
+        await $`npm install ${packageNameLatest}`;
+      }
     }
   });
 });
--- .maintenance/scripts/install-remote.js
+++ .maintenance/scripts/install-remote.js
@@ -3,11 +3,16 @@ import { program } from 'commander';
 
 import { visitPackages } from './helpers/visit-packages.js';
 import { packageScope } from './helpers/variables.js';
+import process from 'node:process';
 
 const options = program.parse().opts();
 
 await visitPackages(options.package, async ({ packageJson }) => {
-  await $`npm install`;
+  if (process.env.CI_DEBUG === '1') {
+    await $`npm install --loglevel verbose`;
+  } else {
+    await $`npm install`;
+  }
 
   const packageNames = Object.keys(packageJson.dependencies ?? {}).filter((x) =>
     x.startsWith(`${packageScope}/`)
@@ -16,7 +21,11 @@ await visitPackages(options.package, async ({ packageJson }) => {
   for (const packageName of packageNames) {
     const packageNameLatest = packageName + '@latest';
 
-    await $`npm install ${packageNameLatest}`;
+    if (process.env.CI_DEBUG === '1') {
+      await $`npm install --loglevel verbose ${packageNameLatest}`;
+    } else {
+      await $`npm install ${packageNameLatest}`;
+    }
   }
 
   await $`npm up --save`;
