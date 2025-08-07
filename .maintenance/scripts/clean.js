602f649cf6d4b311566ef2e071bd8c420b9d6b2b
Hristo Kolev <hristo.a.kolev@gmail.com>
2025-08-07 13:34:02 +0300

    Update examples

--- .maintenance/scripts/clean.js
+++ .maintenance/scripts/clean.js
@@ -1,29 +1,25 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 
+import { $ } from 'zx';
+
 import { init } from './helpers/init.js';
 import { visitNpmPackages } from './helpers/visit-npm-packages.js';
 
 await init();
 
-await visitNpmPackages(async ({ packagePath }) => {
-  const allPaths = [
-    path.join(packagePath, `node_modules`),
-    path.join(packagePath, `dist`),
-    path.join(packagePath, `logs`),
-    path.join(packagePath, `package-lock.json`),
-    path.join(packagePath, `.npmrc`),
-  ];
+$.cwd = path.resolve(import.meta.dirname, '..', '..');
 
-  await Promise.allSettled(
-    allPaths.map(async (dirPath) => {
-      try {
-        await fs.rm(dirPath, { recursive: true, force: true });
-      } catch (error) {
-        console.warn(
-          `\x1b[33m[WARNING] Failed to delete directory "${path.relative(packagePath, dirPath)}"\x1b[0m`
-        );
-      }
-    })
-  );
+await $`git clean -dfX -e "!**/*.local" -e "!.idea/**" -e "!.maintenance/**"`;
+
+await visitNpmPackages(async ({ packagePath }) => {
+  const packageLockPath = path.join(packagePath, 'package-lock.json');
+
+  try {
+    await fs.rm(packageLockPath, { recursive: true, force: true });
+  } catch (error) {
+    console.warn(
+      `\x1b[33m[WARNING] Failed to delete file "${packageLockPath}"\x1b[0m`
+    );
+  }
 });
--- manager-template/2-advanced/README.md
+++ manager-template/2-advanced/README.md
@@ -102,7 +102,7 @@ If you want to experiment with the Kubernetes configurations locally, you can us
 - Minikube and a `kubectl` configured to communicate with your Minikube cluster. Use [this article](https://minikube.sigs.k8s.io/docs/start/) to setup minikube.
 - [WMIC](https://techcommunity.microsoft.com/blog/windows-itpro-blog/how-to-install-wmic-feature-on-demand-on-windows-11/4189530) (on Windows)
 
-- License - **io.Manager** requires a license key to operate. To acquire a license key, contact us at `sales@interop.io`. Apply the license key using the `API_LICENSE_KEY` environment variable in [./kubernetes/_1_environment.yaml](./kubernetes/_1_environment.yaml)
+- License - **io.Manager** requires a license key to operate. To acquire a license key, contact us at `sales@interop.io`. Apply the license key using the `API_LICENSE_KEY` environment variable in [./kubernetes/\_1_environment.yaml](./kubernetes/_1_environment.yaml)
 
 ### Deployment Steps
 
