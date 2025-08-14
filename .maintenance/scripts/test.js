a72c1459c89888bef0eca905599bc253974b28dc
Hristo Kolev <hristo.a.kolev@gmail.com>
2025-08-14 14:45:04 +0300

    Move to workspaces part 1

--- .maintenance/scripts/test.js
+++ .maintenance/scripts/test.js
@@ -10,7 +10,7 @@ import { formatEnvFile } from './helpers/env/format-env-file.js';
 
 await init();
 
-const ignoreList = ['server-template'];
+const ignoreList = ['manager-examples', 'server-template'];
 
 await visitNpmPackages(async ({ packageJson }) => {
   if (ignoreList.includes(packageJson.name)) {
@@ -25,7 +25,7 @@ await visitNpmPackages(async ({ packageJson }) => {
   if (directDependencies.includes('@interopio/manager')) {
     async function test() {
       $.env.__SERVER_INITIALIZATION_TEST__ = 'true';
-      await $`npm run start`;
+      await $`npm run start:node`;
     }
 
     if (await fileExists('.env')) {
